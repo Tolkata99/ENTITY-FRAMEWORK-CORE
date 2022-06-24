247e101f9d58515f3fb7a8c0e8654aacb1bfc281
Anatoli Mihaylov <anatoligithub@gmail.com>
2022-06-24 17:55:29 +0300

    Add tasks from ADO.NET

---
+++ ADO.NET/1. Initial Setup/Config.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._Initial_Setup
+{
+    public static class Config
+    {
+        public const string CONNECTION_STRING = "Server=TOLKATA;Database=MinionsDB;Trusted_Connection=True;";
+    }
+}
+
