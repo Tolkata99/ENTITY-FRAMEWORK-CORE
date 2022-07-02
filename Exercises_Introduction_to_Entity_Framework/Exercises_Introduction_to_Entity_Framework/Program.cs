cd5f51ffff22b5f55c392ce6a000433aeb488490
Anatoli Mihaylov <anatoligithub@gmail.com>
2022-07-02 08:22:01 +0300

    Add tasks from Exercises Introduction to Entity Framework

---
+++ Exercises_Introduction_to_Entity_Framework/Exercises_Introduction_to_Entity_Framework/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Exercises_Introduction_to_Entity_Framework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
