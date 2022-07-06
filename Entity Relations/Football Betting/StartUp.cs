2dfe76b7522484f72dd28cd05cadcd82104bc030
Anatoli Mihaylov <anatoligithub@gmail.com>
2022-07-06 11:28:11 +0300

    Add task from Entity Relations - CODE FIRST

---
+++ Entity Relations/Football Betting/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Football_Betting
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Entity Relations/P03_FootballBetting.Data/Config.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace P03_FootballBetting.Data
+{
+    public static class Config
+    {
+        public const string ConnectionString = @"Server=.;Database=Bet377;Integrated Security=True;";
+    }
+}
