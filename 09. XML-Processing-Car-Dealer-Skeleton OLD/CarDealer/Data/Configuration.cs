90e82cf4cc858a129073d4039048b2cfbde46581
Anatoli Mihaylov <anatoligithub@gmail.com>
2022-07-22 15:53:48 +0300

    Add first task Import Suppliers from Exercises: XML Processing into Database: Car Dealer

---
+++ 09. XML-Processing-Car-Dealer-Skeleton OLD/CarDealer/Data/Configuration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Text;
+
+namespace CarDealer.Data
+{
+    public static class Configuration
+    {
+        public const string ConnectionString = "Server =TOLKATA; Database=CarDealer;Trusted_Connection=True;";
+    }
+}
---
+++ 09. XML-Processing-Car-Dealer-Skeleton OLD/CarDealer/StartUp.cs
@@ -0,0 +1,55 @@
+﻿using System.IO;
+using System.Linq;
+using System.Threading.Channels;
+using System.Xml.Serialization;
+using CarDealer.Dtos.Import;
+using CarDealer.Models;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace CarDealer
+{
+    using Data;
+    using System;
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            CarDealerContext dbContext = new CarDealerContext();
+            //dbContext.Database.EnsureDeleted();
+            //dbContext.Database.EnsureCreated();
+
+            //Console.WriteLine("Database successfully created!");
+
+            string xmlInput = File.ReadAllText(@"../../../Datasets\suppliers.xml");
+           
+            Console.WriteLine(ImportSuppliers(dbContext, xmlInput));
+
+        }
+
+        public static string ImportSuppliers(CarDealerContext context, string inputXml)
+        {
+            XmlRootAttribute root = new XmlRootAttribute("Suppliers");
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(ImportSupplierDto[]), root);
+
+            using StringReader reader = new StringReader(inputXml);
+          
+            var supplierDtos = (ImportSupplierDto[])xmlSerializer
+                 .Deserialize(reader);
+            
+
+            Supplier[] suppliers = supplierDtos
+                .Select(Dto => new Supplier()
+                {
+                    Name = Dto.Name,
+                    IsImporter = Dto.IsImporter
+
+                })
+                .ToArray();
+
+            context.Suppliers.AddRange(suppliers);
+            context.SaveChanges();
+
+            return $"Successfully imported {suppliers.Length}";
+        }
+    }
+}
