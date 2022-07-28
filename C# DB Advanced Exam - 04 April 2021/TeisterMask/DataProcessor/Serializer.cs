790facbc47dc57685d4bbb5dad6b8fa76b182527
Anatoli Mihaylov <anatoligithub@gmail.com>
2022-07-28 11:21:53 +0300

    Add JSON export task from C# DB Advanced Exam - 04 April 2021

--- C# DB Advanced Exam - 04 April 2021/TeisterMask/DataProcessor/Serializer.cs
+++ C# DB Advanced Exam - 04 April 2021/TeisterMask/DataProcessor/Serializer.cs
@@ -1,4 +1,8 @@
-﻿namespace TeisterMask.DataProcessor
+﻿using System.Globalization;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace TeisterMask.DataProcessor
 {
     using System;
 
@@ -15,7 +19,38 @@
 
         public static string ExportMostBusiestEmployees(TeisterMaskContext context, DateTime date)
         {
-            throw new NotImplementedException();
+            var mostBusiestEmployee = context
+                .Employees
+                .ToArray()
+                .Where(e => e.EmployeesTasks.Any(et => et.Task.OpenDate >= date))
+                .Select(e => new
+                {
+                    Username = e.Username.ToString(),
+                    Tasks = e.EmployeesTasks
+                        .Where(et => et.Task.DueDate >= date)
+                        .Select(et=>et.Task)
+                        .OrderByDescending(t=>t.DueDate)
+                        .ThenBy(t=>t.Name)
+                        .Select(s => new
+                        {
+                            TaskName = s.Name,
+                            OpenDate = s.OpenDate.ToString("d",CultureInfo.InvariantCulture),
+                            DueDate = s.DueDate.ToString("d",CultureInfo.InvariantCulture),
+                            LabelType = s.LabelType.ToString(),
+                            ExecutionType = s.ExecutionType.ToString()
+                        })
+                        .ToArray()
+
+
+                })
+                .OrderByDescending(e=>e.Tasks.Length)
+                .ThenBy(u=>u.Username)
+                .Take(10)
+                .ToArray();
+
+            string json = JsonConvert.SerializeObject(mostBusiestEmployee, Formatting.Indented);
+
+            return json;
         }
     }
 }
